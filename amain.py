58cfe471b99125b48823303b1fc6dbadf79d6f13
grucee <546401099@qq.com>
2021-08-12 22:46:27 +0800

    源码运行版本，可在pycharm中进行调试

---
+++ amain.py
@@ -0,0 +1,4 @@
+from manim import __main__
+
+if __name__ == '__main__':
+    __main__.main()
--- manim/__init__.py
+++ manim/__init__.py
@@ -4,7 +4,8 @@
 
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution(__name__).version
+#__version__ = pkg_resources.get_distribution(__name__).version
+__version__ = 'sourcecode-run-version'
 
 
 import sys
